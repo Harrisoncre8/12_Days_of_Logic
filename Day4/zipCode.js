48000d24e24397a4db256a48af3b249127ab0f3c
Harrison Nguyen <nguy2173@umn.edu>
2019-12-26 19:21:41 -0600

    Add day4 folder and problem

---
+++ Day4/zipCode.js
@@ -0,0 +1,18 @@
+function isValid(zip) {
+	let str = Number(zip);
+	if(Number.isInteger(str) && zip.length === 5){
+		return true;
+	} else {
+		return false;
+	}
+}
+
+console.log('*** Testing zip:')
+console.log(`isValid("59001") expected: true, actual:`, isValid("59001"));
+console.log(`isValid("732 32") expected: false, actual:`, isValid("732 32"));
+console.log(`isValid("788876") expected: false, actual:`, isValid("788876"));
+console.log(`isValid("a923b") expected: false, actual:`, isValid("a923b"));
+console.log(`isValid("5923!") expected: false, actual:`, isValid("5923!"));
+console.log(`isValid("88231") expected: true, actual:`, isValid("88231"));
+
+
