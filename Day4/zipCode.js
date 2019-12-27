d4b5af2293f698f6c7cf056bc290acd83f6a5946
Harrison Nguyen <nguy2173@umn.edu>
2019-12-27 17:10:47 -0600

    Add day5 folder

--- Day4/zipCode.js
+++ Day4/zipCode.js
@@ -14,5 +14,3 @@ console.log(`isValid("788876") expected: false, actual:`, isValid("788876"));
 console.log(`isValid("a923b") expected: false, actual:`, isValid("a923b"));
 console.log(`isValid("5923!") expected: false, actual:`, isValid("5923!"));
 console.log(`isValid("88231") expected: true, actual:`, isValid("88231"));
-
-
