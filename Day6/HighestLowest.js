0006171e502540fa9639e32ef8c04fba554a6f3c
Harrison Nguyen <nguy2173@umn.edu>
2019-12-29 13:01:42 -0600

    Add testing

--- Day6/HighestLowest.js
+++ Day6/HighestLowest.js
@@ -4,3 +4,12 @@ function highLow(str) {
     let lowest = Math.min(...integer);
     return `${highest} ${lowest}`
 }
+
+console.log('*** Testing sumOfCubes:')
+console.log(`highLow("1 -1") expected: "1 -1" actual:`, highLow("1 -1"));
+console.log(`highLow("-1 -1") expected: "-1 -1" actual:`, highLow("-1 -1"));
+console.log(`highLow("42") expected: "42 42" actual:`, highLow("42"));
+console.log(`highLow("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6") expected: "542 -214" 
+            actual:`, highLow("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6"));
+console.log(`highLow("-1 -1 0") expected: "0 -1" actual:`, highLow("-1 -1 0"));
+console.log(`highLow("1 -1 0") expected: "1 -1" actual:`, highLow("1 -1 0"));
