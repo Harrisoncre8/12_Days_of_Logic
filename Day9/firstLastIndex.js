e76d847698d5e17bed6447192f9e232afdfbce95
Harrison Nguyen <nguy2173@umn.edu>
2020-01-01 19:39:44 -0600

    Add testing for day9

--- Day9/firstLastIndex.js
+++ Day9/firstLastIndex.js
@@ -5,3 +5,10 @@ function charIndex(word, char) {
         return [word.indexOf(char), word.lastIndexOf(char)]
     }
 }
+
+console.log('*** Testing charIndex:')
+console.log(`charIndex('hello', 'l') expected: [2, 3], actual:`, charIndex('hello', 'l'));
+console.log(`charIndex('circumlocution', 'r') expected: [2, 2], actual:`, charIndex('circumlocution', 'r'));
+console.log(`charIndex('happy', 'e') expected: undefined, actual:`, charIndex('happy', 'e'));
+console.log(`charIndex('happy', 'p') expected: [2, 3], actual:`, charIndex('happy', 'p'));
+console.log(`charIndex('happy', 'h') expected: [0, 0], actual:`, charIndex('happy', 'h'));
