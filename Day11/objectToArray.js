2add4565187a61a5f20b4cc6e2c10a7bcaeea5cc
Harrison Nguyen <nguy2173@umn.edu>
2020-01-03 19:14:45 -0600

    Add solution to day11

--- Day11/objectToArray.js
+++ Day11/objectToArray.js
@@ -1,3 +1,10 @@
 function objectToArray(obj) {
-	
+	return Object.entries(obj)
 }
+
+console.log('*** Testing upwardTrend:')
+console.log(`objectToArray({D: 1, B: 2, C: 3}) expected: [["D", 1], ["B", 2], ["C", 3]], 
+    actual:`, objectToArray({D: 1, B: 2, C: 3}));
+console.log(`objectToArray({likes: 2, dislikes: 3, followers: 10 
+    expected: [["likes", 2], ["dislikes", 3], ["followers", 10]], 
+    actual:`, objectToArray({likes: 2, dislikes: 3, followers: 10}));
