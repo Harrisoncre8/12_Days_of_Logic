8c76f17fe00c4593c092216b9aa6a63eaada4a9a
Harrison Nguyen <nguy2173@umn.edu>
2020-01-03 19:15:28 -0600

    Add changes to testing

--- Day11/objectToArray.js
+++ Day11/objectToArray.js
@@ -2,7 +2,7 @@ function objectToArray(obj) {
 	return Object.entries(obj)
 }
 
-console.log('*** Testing upwardTrend:')
+console.log('*** Testing objectToArray:')
 console.log(`objectToArray({D: 1, B: 2, C: 3}) expected: [["D", 1], ["B", 2], ["C", 3]], 
     actual:`, objectToArray({D: 1, B: 2, C: 3}));
 console.log(`objectToArray({likes: 2, dislikes: 3, followers: 10 
