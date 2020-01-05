12a7a814cf3b4ab5f50e4f7a48a85fc283ee3eed
Harrison Nguyen <nguy2173@umn.edu>
2020-01-04 22:42:59 -0600

    Add solution and testing for day12

--- Day12/README.md
+++ Day12/README.md
@@ -1,8 +1,10 @@
 Create a function that takes an array of numbers and returns a new array, sorted in ascending order (smallest to biggest).
 
-> * Sort numbers array in ascending order.
-> * If functions argument is null, an empty array or undefined, return an empty array.
-> * Return new array of sorted numbers.
+* Sort numbers array in ascending order.
+* If functions argument is null, an empty array or undefined, return an empty array.
+* Return new array of sorted numbers.
+
+> The numbers being passed to sortNumsAscending() can be positive or negative.
 
 Examples
 
--- Day12/sortNumberAscending.js
+++ Day12/sortNumberAscending.js
@@ -1,3 +1,18 @@
 function sortNumsAscending(arr) {
-	
+	if(Array.isArray(arr) !== true){
+		return []
+    };
+    let newArr = arr.sort((a, b) => a - b);
+    return newArr;
 }
+
+console.log('*** Testing sortNumsAscending:')
+console.log(`sortNumsAscending([1, 2, 10, 50, 5]) expected: [1, 2, 5, 10, 50], 
+             actual:`, sortNumsAscending([1, 2, 10, 50, 5]));
+console.log(`sortNumsAscending(null) expected: [], actual:`, sortNumsAscending(null));
+console.log(`sortNumsAscending([]) expected: [], actual:`, sortNumsAscending([]));
+console.log(`sortNumsAscending([-23, -69, -54, -2, -32]) expected: [-69, -54, -32, -23, -2], 
+             actual:`, sortNumsAscending([-23, -69, -54, -2, -32]));
+console.log(`sortNumsAscending([-21, -9, -96]) expected: [-96, -21, -9], 
+             actual:`, sortNumsAscending([-21, -9, -96]));
+console.log(`sortNumsAscending([0]) expected: [0], actual:`, sortNumsAscending([0]));
